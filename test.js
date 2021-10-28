56fd47b733b15c4ca3ea86b152559eba9407231b
gbjd <wx807663414@qq.com>
2021-10-28 21:33:32 +0800

    class

--- test.js
+++ test.js
@@ -1,5 +1,20 @@
-function myFunction(x = 1, y = 2, z = 3) {
-    console.log(x, y, z);
+function Person(){
+    this.name;
+}
+Person.prototype.say=function(){
+    console.log("hello");
 }
 
-myFunction(3, 5);
+var person=new Person();
+
+console.log(Person.__proto__); // 指向Function.prototype
+console.log(Function.prototype);
+
+console.log(Person.prototype.__proto__); // 指向Object.prototype
+console.log(Object.prototype);
+
+console.log(person.__proto__); // 指向Person.prototype
+console.log(Person.prototype);
+
+console.log(Person.prototype.constructor); // 指向Person()
+console.log(Person);
